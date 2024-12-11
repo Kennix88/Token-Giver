7c57e581e7c1a62c5237734c5e3ee275b4eaeaa6
Kennix88 <shel.live@yandex.ru>
2024-12-11 18:38:35 +0800

    Update

--- src/app/(telegram)/_components/Navbar.tsx
+++ src/app/(telegram)/_components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div className="flex justify-between items-center sticky  top-0 py-2 bg-black bg-opacity-50 backdrop-blur-md">
       <p className="text-white text-xl font-bold font-montserrat">
-        Rats Kingdom
+        {process.env.PROJECT_NAME}
       </p>
 
       {/*<TonConnectButton />*/}
