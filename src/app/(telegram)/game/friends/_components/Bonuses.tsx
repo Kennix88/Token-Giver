3759171c00d0f70696ca08dd1e5f255eae3f0e78
Kennix88 <shel.live@yandex.ru>
2025-01-26 16:41:16 +0800

    add icons

---
+++ src/app/(telegram)/game/friends/_components/Bonuses.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Bonuses() {
+  return (
+    <div className="flex flex-col gap-1 font-extralight">
+      <div className="px-4 opacity-50 flex flex-row gap-2 items-center">
+        Бонусы
+      </div>
+      <div className="bg-surface-container-l2 p-4 rounded-md flex flex-col gap-4 text-sm bg-opacity-70"></div>
+    </div>
+  )
+}
--- src/const/IconsList.tsx
+++ src/const/IconsList.tsx
@@ -5,6 +5,7 @@ import wallet from '@/app/_assets/icons/wallet.svg'
 import { IconsEnum } from '@/types/icons.enum.ts'
 import Image from 'next/image'
 import { BsFillEmojiWinkFill, BsTwitterX } from 'react-icons/bs'
+import { FaPercentage } from 'react-icons/fa'
 import {
   FaBoltLightning,
   FaDiscord,
@@ -14,6 +15,7 @@ import {
   FaPlay,
   FaRobot,
   FaTiktok,
+  FaUser,
   FaViber,
   FaWhatsapp,
   FaYoutube,
@@ -30,7 +32,6 @@ export interface IconInterface {
   title: string
   icon: JSX.Element
   color: string
-  twColor: string
 }
 export const IconsList: IconInterface[] = [
   {
@@ -38,167 +39,161 @@ export const IconsList: IconInterface[] = [
     title: 'Other',
     icon: <MdWebAsset className="text-2xl" />,
     color: '#211F26',
-    twColor: 'surface-container',
   },
   {
     key: IconsEnum.emoji,
     title: 'Emoji',
     icon: <BsFillEmojiWinkFill className="text-2xl" />,
     color: '#211F26',
-    twColor: 'surface-container',
   },
   {
     key: IconsEnum.telegram,
     title: 'Telegram',
     icon: <RiTelegram2Fill className="text-2xl" />,
     color: '#26A5E4',
-    twColor: 'social-telegram',
   },
   {
     key: IconsEnum.wallet,
     title: 'Wallet',
     icon: <Image src={wallet} alt={'wallet'} width={24} height={24} />,
     color: '#4C9CE2',
-    twColor: 'social-wallet',
   },
   {
     key: IconsEnum.ton,
     title: 'Ton',
     icon: <Image src={ton} alt={'ton'} width={24} height={24} />,
     color: '#0098EA',
-    twColor: 'social-ton',
   },
   {
     key: IconsEnum.ads,
     title: 'Ads',
     icon: <FaPlay className="text-2xl" />,
     color: '#fe0101',
-    twColor: 'social-ads',
   },
   {
     key: IconsEnum.boost,
     title: 'Boost',
     icon: <FaBoltLightning className="text-2xl" />,
     color: '#2276f4',
-    twColor: 'social-boost',
+  },
+  {
+    key: IconsEnum.percent,
+    title: 'Percent',
+    icon: <FaPercentage className="text-2xl" />,
+    color: '#2276f4',
+  },
+  {
+    key: IconsEnum.friend,
+    title: 'Friend',
+    icon: <FaUser className="text-2xl" />,
+    color: '#34c759',
+  },
+  {
+    key: IconsEnum.premiumFriend,
+    title: 'premiumFriend',
+    icon: <FaUser className="text-2xl" />,
+    color: '#FFD700',
   },
   {
     key: IconsEnum.friends,
     title: 'Friends',
     icon: <Image src={friends} alt={'friends'} width={24} height={24} />,
     color: '#34c759',
-    twColor: 'social-friends',
   },
   {
     key: IconsEnum.age,
     title: 'Age',
     icon: <LuSprout className="text-2xl" />,
     color: '#34c759',
-    twColor: 'social-age',
   },
   {
     key: IconsEnum.premium,
     title: 'Premium',
     icon: <HiStar className="text-2xl" />,
     color: '#ffe500',
-    twColor: 'social-premium',
   },
   {
     key: IconsEnum.star,
     title: 'Star',
     icon: <Image src={tgStar} alt={'tg star'} width={24} height={24} />,
     color: '#ffe500',
-    twColor: 'social-star',
   },
   {
     key: IconsEnum.robot,
     title: 'Robot',
     icon: <FaRobot className="text-2xl" />,
     color: '#1E5D88',
-    twColor: 'social-robot',
   },
   {
     key: IconsEnum.x,
     title: 'X',
     icon: <BsTwitterX className="text-2xl" />,
     color: '#000000',
-    twColor: 'social-x',
   },
   {
     key: IconsEnum.youtube,
     title: 'Youtube',
     icon: <FaYoutube className="text-2xl" />,
     color: '#FF0000',
-    twColor: 'social-youtube',
   },
   {
     key: IconsEnum.tiktok,
     title: 'Tiktok',
     icon: <FaTiktok className="text-2xl" />,
     color: '#000000',
-    twColor: 'social-tiktok',
   },
   {
     key: IconsEnum.instagram,
     title: 'Instagram',
     icon: <FaInstagram className="text-2xl" />,
     color: '#FF0069',
-    twColor: 'social-instagram',
   },
   {
     key: IconsEnum.facebook,
     title: 'Facebook',
     icon: <FaFacebookF className="text-2xl" />,
     color: '#0866FF',
-    twColor: 'social-facebook',
   },
   {
     key: IconsEnum.boosty,
     title: 'Boosty',
     icon: <SiBoosty className="text-2xl" />,
     color: '#F15F2C',
-    twColor: 'social-boosty',
   },
   {
     key: IconsEnum.discord,
     title: 'Discord',
     icon: <FaDiscord className="text-2xl" />,
     color: '#5865F2',
-    twColor: 'social-discord',
   },
   {
     key: IconsEnum.binance,
     title: 'Binance',
     icon: <SiBinance className="text-2xl" />,
     color: '#F0B90B',
-    twColor: 'social-binance',
   },
   {
     key: IconsEnum.vk,
     title: 'VK',
     icon: <IoLogoVk className="text-2xl" />,
     color: '#0077FF',
-    twColor: 'social-vk',
   },
   {
     key: IconsEnum.github,
     title: 'GitHub',
     icon: <FaGithub className="text-2xl" />,
     color: '#181717',
-    twColor: 'social-github',
   },
   {
     key: IconsEnum.viber,
     title: 'Viber',
     icon: <FaViber className="text-2xl" />,
     color: '#7360F2',
-    twColor: 'social-viber',
   },
   {
     key: IconsEnum.whatsapp,
     title: 'Whatsapp',
     icon: <FaWhatsapp className="text-2xl" />,
     color: '#25D366',
-    twColor: 'social-whatsapp',
   },
 ]
