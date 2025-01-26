7eb573bf9f1347321756740b450aa4ef811d6265
Kennix88 <shel.live@yandex.ru>
2025-01-26 14:45:10 +0800

    add title liaders

--- src/app/(telegram)/game/leaders/_components/Title.tsx
+++ src/app/(telegram)/game/leaders/_components/Title.tsx
@@ -3,13 +3,10 @@
 import { useTranslations } from 'use-intl'
 
 export default function Title() {
-  const t = useTranslations('game.tasks')
+  const t = useTranslations('game.leaders')
   return (
     <div className="flex flex-col gap-2 uppercase">
       <div className="text-2xl font-bold ">{t('title')}</div>
-      <div className="text-lg font-medium">
-        {t('get')} <span className="opacity-80 font-normal">{t('for')}</span>
-      </div>
     </div>
   )
 }
